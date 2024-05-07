395226bdbd32ea8ff5a2507d30164d8a6f90f820
Max-melon <130572955+Max-melon@users.noreply.github.com>
2024-05-07 09:28:13 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pygame
 import random
 import time
+from shooter import Shooter
 
 pygame.init()
 pygame.font.init()
 my_font = pygame.font.SysFont('Comic Sans', 20)
 pygame.display.set_caption("Space Shooter!")
 bg = pygame.image.load("space_background.jpg")
-
+rocket = Shooter(460,40)
 
 size = (800, 600)
 screen = pygame.display.set_mode(size)
@@ -17,9 +18,14 @@ run = True
 frame = 0
 while run:
     keys = pygame.key.get_pressed()
-    # if keys[pygame.K_SPACE]:
+    if keys[pygame.K_d]:
+        rocket.move_right(10)
+
+
+
     clock.tick(60)
     screen.blit(bg, (0, 0))
+    screen.blit(rocket.image, rocket.rect)
     pygame.display.update()
 
 
--- shooter.py
+++ shooter.py
@@ -1,2 +1,20 @@
+import pygame
+
+
 class Shooter:
+
+    def __init__(self, x, y):
+        self.y = y
+        self.x = x
+        self.image = pygame.image.load("rocket.jpg")
+        self.image_size = self.image.get_size()
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+
+    def move_left(self, new_x):
+        self.x -= new_x
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+
+    def move_right(self, new_x):
+        self.x += new_x
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
 
